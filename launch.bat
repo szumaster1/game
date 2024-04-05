e44976639f159c634ee899de381e9fbcd028f925
szumaster <166091306+szumaster3@users.noreply.github.com>
2024-04-05 17:57:59 +0200

    Initial commit

---
+++ launch.bat
@@ -0,0 +1,22 @@
+@echo off
+title 2009scape console log
+
+set _JAVA_OPTIONS=
+
+netstat -an | findstr "0.0.0.0:43595" > NUL
+if %ERRORLEVEL% equ 0 ( 
+  echo "Port 43595 is in use. Cannot start the server again."
+  pause
+  exit
+)
+
+pushd %~dp0\game
+  start /b %~dp0\jre\bin\java.exe -Xmx2560m -Xms2560m -jar server.jar
+
+:waitserver
+  netstat -an | findstr "0.0.0.0:43595" > NUL
+  if %ERRORLEVEL% equ 1 ( 
+    echo Still waiting for the server to start...
+    timeout 2 > NUL
+    goto waitserver
+)
